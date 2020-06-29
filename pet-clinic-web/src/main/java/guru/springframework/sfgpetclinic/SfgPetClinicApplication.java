b9599dcf5a85e236715ead8511bace2a7ebfdb36
mak199 <mak.khan199@googlemail.com>
2020-06-29 12:43:20 +0200

    Adding static resources. closes #17

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -3,6 +3,7 @@ package guru.springframework.sfgpetclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Spring context
 @SpringBootApplication
 public class SfgPetClinicApplication {
     public static void main(String[] args){
