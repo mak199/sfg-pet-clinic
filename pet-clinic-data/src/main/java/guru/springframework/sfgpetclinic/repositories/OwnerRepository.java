bab064d868743644c0fa0bb6fcfcb18beffc9ef5
mak199 <mak.khan199@googlemail.com>
2020-07-08 10:55:58 +0200

    Implement Find Owner.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,6 +3,13 @@ package guru.springframework.sfgpetclinic.repositories;
 import guru.springframework.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner,Long> {
     Owner findByLastName(String lastName);
+
+    //@Query("SELECT u FROM Owner u WHERE u.lastName = :lastName")
+    List<Owner> findAllByFirstNameLike(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
@@ -8,6 +8,7 @@ import guru.springframework.sfgpetclinic.services.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 // We want map implementation of CRUDService interface to be brought to spring context as a spring bean
@@ -79,5 +80,15 @@ public class OwnerMapService extends AbstractMapService<Owner,Long> implements O
                 .filter(owner->owner.getLastName()
                 .equalsIgnoreCase(lastName))
                 .findFirst().orElse(null);
+    }
+
+    @Override
+    public List<Owner> findAllByFirstNameLike(String lastName) {
+        return null;
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName) {
+        return null;
     }
 }
